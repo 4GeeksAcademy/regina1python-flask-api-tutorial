91a73f29b91820dfcbf2c750cae63bf5f769ad4d
Regina7411 <danielagg71411@gmail.com>
2025-04-17 22:00:46 +0000

    correcion

--- src/app.py
+++ src/app.py
@@ -18,19 +18,16 @@ def add_new_todo():
 
 @app.route('/todos/<int:position>', methods=['DELETE'])
 def delete_todo(position):
-    print("This is the position to delete:", position)
-    return 'something'
-
-    """try:
+    try:
         todos.pop(position)
         return jsonify(todos)
     except IndexError:
         return jsonify({"error": "Índice fuera de rango"}), 404
-"""
-"""@app.route('/todos', methods=['DELETE'])
+
+@app.route('/todos', methods=['DELETE'])
 def delete_all_todos():
     todos.clear()
-  return jsonify(todos)
-"""
+    return jsonify(todos)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
